6ef38a6870d0b24e5c07054c358df0e0e09f3871
llenotre <llenotre@student.42.fr>
2021-04-22 16:14:46 +0200

    Hello module fix

--- assignment01/hello.c
+++ assignment01/hello.c
@@ -15,13 +15,13 @@ MODULE_VERSION("0.1");
 
 static int __init hello_init(void)
 {
-	pr_info("Hello world!");
+	pr_info("Hello world!\n");
 	return 0;
 }
 
 static void __exit hello_exit(void)
 {
-	pr_info("Cleaning up module.");
+	pr_info("Cleaning up module.\n");
 }
 
 module_init(hello_init);
