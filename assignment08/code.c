e0375b0668dd32fbf5e29ccd9fe67abf93068ecb
llenotre <llenotre@student.42.fr>
2021-04-23 23:04:31 +0200

    Added Makefile for assignment08 and fixed compilation/code style

--- assignment08/code.c
+++ assignment08/code.c
@@ -11,17 +11,14 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Louis Solofrizzo <louis@ne02ptzero.me>");
 MODULE_DESCRIPTION("Useless module");
 
-static ssize_t myfd_read
-(struct file *fp, char__user * user,
-size_t size, loff_t *offs);
-static ssize_tmyfd_write(struct file *fp, const char__user * user,
-size_t size, loff_t *offs);
+static ssize_t myfd_read(struct file *fp, char __user *user, size_t size, loff_t *offs);
+static ssize_t myfd_write(struct file *fp, const char __user *user, size_t size, loff_t *offs);
 
 const static struct file_operations   myfd_fops = {
 	.owner = THIS_MODULE, .read    = &myfd_read, .write = &myfd_write
 };
 
-static struct miscdevicemyfd_device = {
+static struct miscdevice myfd_device = {
 	.minor = MISC_DYNAMIC_MINOR, .name = "reverse",
 	.fops = &myfd_fops
 };
@@ -41,7 +38,7 @@ static void __exit myfd_cleanup(void)
 {
 }
 
-ssize_t myfd_read(struct file *fp, char__user * user, size_t size, loff_t *offs)
+ssize_t myfd_read(struct file *fp, char __user *user, size_t size, loff_t *offs)
 {
 	size_t t, i;
 	char *tmp2;
@@ -55,7 +52,7 @@ ssize_t myfd_read(struct file *fp, char__user * user, size_t size, loff_t *offs)
 	return simple_read_from_buffer(user, size, offs, tmp, i);
 }
 
-ssize_t myfd_write(struct file *fp, const char__user * user, size_t size, loff_t *offs)
+ssize_t myfd_write(struct file *fp, const char __user *user, size_t size, loff_t *offs)
 {
 	ssize_t res;
 
