f654c262e6c7a9cbc5b6f34f60cd6d9cdfc1c7e7
llenotre <llenotre@student.42.fr>
2021-05-19 14:59:00 +0200

    Memory leak fix

--- assignment08/code.c
+++ assignment08/code.c
@@ -15,41 +15,50 @@ static ssize_t myfd_read(struct file *fp, char __user *user, size_t size, loff_t
 static ssize_t myfd_write(struct file *fp, const char __user *user, size_t size, loff_t *offs);
 
 const static struct file_operations   myfd_fops = {
-	.owner = THIS_MODULE, .read    = &myfd_read, .write = &myfd_write
+	.owner = THIS_MODULE,
+	.read    = &myfd_read,
+	.write = &myfd_write
 };
 
 static struct miscdevice myfd_device = {
-	.minor = MISC_DYNAMIC_MINOR, .name = "reverse",
+	.minor = MISC_DYNAMIC_MINOR,
+	.name = "reverse",
 	.fops = &myfd_fops
 };
 
 char str[PAGE_SIZE];
-char *tmp;
 
 static int __init myfd_init(void)
 {
 	int retval;
 
-	retval = misc_register(&(*(&(myfd_device))));
-	return 1;
+	retval = misc_register(&myfd_device);
+	if (retval < 0) {
+		return 1;
+	}
+	return 0;
 }
 
 static void __exit myfd_cleanup(void)
 {
+	misc_deregister(&myfd_device);
 }
 
 ssize_t myfd_read(struct file *fp, char __user *user, size_t size, loff_t *offs)
 {
 	size_t t, i;
-	char *tmp2;
+	char *tmp;
+	ssize_t res;
 
 	/**************** Malloc like a boss***************/
-	tmp2 = kmalloc(sizeof(char) * PAGE_SIZE * 2, GFP_KERNEL);
-	tmp = tmp2;
+	tmp = kmalloc(sizeof(char) * PAGE_SIZE * 2, GFP_KERNEL);
 	for (t = strlen(str) - 1, i = 0; t >= 0; t--, i++)
 		tmp[i] = str[t];
 	tmp[i] = 0x0;
-	return simple_read_from_buffer(user, size, offs, tmp, i);
+
+	res = simple_read_from_buffer(user, size, offs, tmp, i);
+	kfree(tmp);
+	return res;
 }
 
 ssize_t myfd_write(struct file *fp, const char __user *user, size_t size, loff_t *offs)
@@ -58,7 +67,8 @@ ssize_t myfd_write(struct file *fp, const char __user *user, size_t size, loff_t
 
 	res = 0;
 	res = simple_write_to_buffer(str, size, offs, user, size) + 1;
-	// 0x0 = ’\0’str[size + 1] = 0x0;
+	// 0x0 = ’\0’
+	str[size + 1] = 0x0;
 	return res;
 }
 
